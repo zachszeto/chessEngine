edcfffd6dc6bd79506d27ad8404f5ba50676a0d5
zachszeto <zacharyszto@gmail.com>
2023-07-30 23:26:53 -0400

    Update Castle Perms

--- board.c
+++ board.c
@@ -68,6 +68,42 @@ int Parse_Fen(char *fen, S_BOARD *pos){
     }
     fen++;
   }
+
+  ASSERT(*fen == 'w' || *fen == 'b');
+
+  pos->side = (*fen == 'w') ? WHITE : BLACK;
+  fen += 2;
+
+  for (i = 0; i < 4; i++){
+    if (*fen == ' '){
+      break;
+    }
+    switch(*fen){
+      case 'K': pos->castlePerm != WKCA; break;
+      case 'Q': pos->castlePerm != WQCA; break;
+      case 'k': pos->castlePerm != BKCA; break;
+      case 'q': pos->castlePerm != BQCA; break;
+      default: break;
+    }
+    fen++;
+  }
+  fen++;
+
+  ASSERT(pos->castlePerm>=0 && pos->castlePerm<=15);
+
+  if (*fen != '-'){
+    file = fen[0] - 'a';
+    rank = fen[1] - '1';
+
+    ASSERT(file>=FILE_A && file<=FILE_H);
+    ASSERT(rank>= RANK_1 && file<=RANK_8);
+
+    pos->enPas = FR2SQ(file,rank);
+  }
+
+  pos->posKey = GeneratePosKey(pos);
+
+  return 0;
 }
 
 // Reset the board to the initial starting position
